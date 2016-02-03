95641e0c1c37bd4ab84a5d397df9fd5ea9dc60e6
Aldrin Thivyanathan <aldrin.a.thivyanathan@pwc.com>
2016-02-03 23:07:16 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/DemoPodCreate/DemoPodCreate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DemoPodCreate : NSObject
+@end
+@implementation PodsDummy_DemoPodCreate
+@end
---
+++ Example/Pods/Target Support Files/DemoPodCreate/DemoPodCreate-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double DemoPodCreateVersionNumber;
+FOUNDATION_EXPORT const unsigned char DemoPodCreateVersionString[];
+
